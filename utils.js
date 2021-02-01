2cc02d4465b2bb6ae582815965d1299b79668cbc
NHACHQ <NHACHQ@outlook.com>
2021-02-01 03:43:38 -0500

    restructured stimuli, added practice trial

--- utils.js
+++ utils.js
@@ -18,3 +18,12 @@ function imgSelect(start, end, interval, path){
     }
     return [images, images_r];
 }
+
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
