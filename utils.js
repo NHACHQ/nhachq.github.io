ccd4974c9fe58fbd6c6399bf0cb082894df7c2a4
NHACHQ <nhachq@outlook.com>
2021-10-15 08:47:12 -0400

    half done random functions

--- utils.js
+++ utils.js
@@ -39,7 +39,7 @@ function shuffle(array) {
 
 //for abrupt trials
 // parameters: paths of two iamges
-// change point : a decimal number between 0 and 1
+// change point : where the sudden change happens
 // num: overall number of frames
 // return a set of images with two directions
 function abrupt_img(start_path, end_path, change, num){
@@ -61,4 +61,50 @@ function abrupt_img(start_path, end_path, change, num){
 
     }
     return images;
+}
+
+// this function is for setting up stimuli for random trials
+// each time the targe stumuli got 
+
+
+
+
+function random_select(paths, trials){
+    var randoms = [];
+    var len = paths[0][0].length;
+    //this is a timeline variable 2d array
+    var final = [];
+    var temp;
+    
+    //get a 1D array
+    for (var i = 0; i<trials; i++){
+        temp = paths[i][0];
+        var temp_1 = [];
+        for (var j = 0; j<temp.length; j++){
+            temp_1.push({stimulus: temp[i][j], data: {trial_tag: 'random-'+i+'-'+j} });
+            //randoms[i%3].push({stimulus: temp[i][j], data: {trial_tag: 'random-'+i+'-'+j} });
+        }
+        randoms[i%3]=temp_1;
+    }
+
+    // randomize all images
+    for (var j = 0; j < randoms.length; j++){
+
+        for (let i = randoms[j].length -1; i > 0; i--) {
+            let p = Math.floor(Math.random() * i);
+            let k = randoms[j][i];
+            randoms[j][i] = randoms[j][p];
+            randoms[j][p] = k;
+          }
+
+    }
+
+
+    // // divide into number of trials
+    // for (var i = 0; i<trials; i++){
+    //     temp = randoms.slice(i*len, (i+1)*len);
+    //     final.push(temp);
+    // }
+
+    return randoms;
 }
